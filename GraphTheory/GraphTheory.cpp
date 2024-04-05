70281eff7c4c324f56ab2c8ddc715d2935a935ca
linhnguyen15492 <84197075+linhnguyen15492@users.noreply.github.com>
2024-04-05 13:24:37 +0700

    commit

--- GraphTheory/GraphTheory.cpp
+++ GraphTheory/GraphTheory.cpp
@@ -10,9 +10,6 @@ using namespace std;
 
 int welcome()
 {
-	int choice;
-
-
 	cout << "Do an Ly thuyet do thi - HK2/2023-2024 - Nhom DA-01" << endl;
 	cout << "1. Yeu cau 1: Kiem tra do thi co phai la do thi vo huong hay khong?" << endl;
 	cout << "2. Yeu cau 2: Kiem tra do thi co phai la do thi vo huong co huong hay khong?" << endl;
@@ -20,88 +17,72 @@ int welcome()
 	cout << "4. Yeu cau 4: Kiem tra do thi co phai la do thi vo huong co trong so hay khong?" << endl;
 	cout << "5. Yeu cau 5: Kiem tra do thi co phai la do thi co huong co trong so hay khong?" << endl;
 	cout << "6. Thoat chuong trinh" << endl;
-	cout << "Chon yeu cau: ";
 
-	cin >> choice;
-	bool flag = choice < 1 || choice > 6;
-	while (flag)
+	string choice;
+	bool flag = true;
+	do
 	{
-		cout << "Yeu cau khong hop le" << choice << endl;
 		cout << "Chon yeu cau: ";
+
+		int v;
 		cin >> choice;
+		try {
+			v = stoi(choice);
+			cout << "Ban da chon: " << v << endl;
 
-		flag = choice < 1 || choice > 6;
-	}
-
-
-	return choice;
+			flag = v < 1 || v > 6;
+			//cout << "flag: " << flag << endl;
+			if (flag)
+			{
+				cout << "Yeu cau khong hop le!" << endl;
+			}
+			else
+			{
+				cout << "Yeu cau hop le!" << endl;
+				return v;
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Yeu cau khong hop le!" << endl;
+		}
+	} while (flag);
 }
-
-/*
-switch(expression) {
-  case x:
-	// code block
-	break;
-  case y:
-	// code block
-	break;
-  default:
-	// code block
-}
-*/
 
 
 // Driver Code
 int main()
 {
-	//cout << "Do an Ly thuyet do thi - HK2/2023-2024 - Nhom DA-01" << endl;
-
-	//int choice = welcome();
-
-
-	char guess;
+	int choice;
 	do
 	{
-		cout << "Guess a number between 1 and 10 ? " << endl;
+		choice = welcome();
 
-		cin >> guess;
-
-		guess = int(guess);
-
-		cout << "You guessed: " << guess << endl;
-
-	} while (guess < 1 || guess > 10);
-
-	//bool flag = true;
-	//while (true)
-	//{
-	//	int choice = welcome();
-	//	switch (choice)
-	//	{
-	//	case 1:
-	//		yeuCau1();
-	//		break;
-	//	case 2:
-	//		yeuCau2();
-	//		break;
-	//	case 3:
-	//		yeuCau3();
-	//		break;
-	//	case 4:
-	//		yeuCau4();
-	//		break;
-	//	case 5:
-	//		yeuCau5();
-	//		break;
-	//	case 6:
-	//		cout << "Ket thuc chuong trinh!" << endl;
-	//		flag = false;
-	//		break;
-	//	default:
-	//		cout << "Yeu cau khong hop le!" << endl;
-	//		break;
-	//	}
-	//}
+		switch (choice)
+		{
+		case 1:
+			yeuCau1();
+			break;
+		case 2:
+			yeuCau2();
+			break;
+		case 3:
+			yeuCau3();
+			break;
+		case 4:
+			yeuCau4();
+			break;
+		case 5:
+			yeuCau5();
+			break;
+		case 6:
+			cout << "Ket thuc chuong trinh!" << endl;
+			break;
+		default:
+			//cout << "Yeu cau khong hop le!" << endl;
+			break;
+		}
+	} while (choice != 6);
 
 	//yeuCau1();
 
@@ -112,8 +93,6 @@ int main()
 	//yeuCau4();
 
 	//yeuCau5();
-
-	//cout << "Ket thuc chuong trinh!" << endl;
 
 	return 0;
 }
